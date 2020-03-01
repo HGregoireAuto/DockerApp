48367c8d6fb8ea3a8dc2030dfcd97334fc4b5ac1
Yanik Lupien <ylupien@hgreg.ca>
2020-03-01 15:06:16 -0500

    First docker commit

---
+++ public/index.php
@@ -0,0 +1,24 @@
+<?php 
+//phpinfo();
+
+$user = 'root';
+$pass = '1q2w3e';
+
+$conn = new PDO('mysql:host=mariadb;dbname=bbb', $user, $pass);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+
+
+$redis = new Redis();
+$redis->connect('redis', 6379);
+print "<pre>";
+print_r($redis->info());
+print "</pre>";
+ 
+if ($redis->ping()) {
+  echo "PONG\n";
+}
+
